d0a26f98344af0cba51c81213de9bac02a63dc36
JulianSabarino <julian.sabarino@gmail.com>
2022-03-27 19:38:07 -0300

    Ejercicios 4 completo, 5 incompleto

---
+++ Ejercicio I04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio_I04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int acNum = 1;
+            do
+            {
+                if (isPerfect(acNum))
+                {
+                    Console.WriteLine(acNum);
+                    cont++;
+                }
+                acNum++;
+//                Console.WriteLine(acNum);
+//                Console.ReadKey();
+            } while (cont < 4);
+        }
+
+        static bool isPerfect(int num)
+        {
+            bool isPerfect = false;
+            int acumulator = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    acumulator=acumulator + i;
+                if (acumulator > num)
+                    break;
+            }
+
+            if (acumulator == num)
+                isPerfect = true;
+
+            return isPerfect;
+        }
+    }
+}
---
+++ Ejercicio I05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_I05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Ingrese un numero entero positivo");
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num <= 0)
+                    Console.WriteLine("Ingrese un numero valido");
+
+            } while (num <= 0);
+
+            Program.PrintCenter(num);
+
+        }
+
+        static void PrintCenter(int num)
+        {
+            int cont = 0;
+
+            int acHi = num;
+            int acLow = 0;
+/*
+            do
+            {
+                acHi = acHi + (acHi - 1);
+                acLow = acLow + (acLow + 1);
+                cont++;
+
+                Console.WriteLine("Hi:{0} Low:{1} cont:{2}",acHi, acLow,cont);
+                Console.ReadKey();
+
+            }while (acLow < acHi && cont < num);
+*/
+            Console.WriteLine(cont);
+        }
+
+        static int FindCenter(int num)
+        {
+            return 0;
+        }
+    }
+}
